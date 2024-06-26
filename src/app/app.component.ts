d8a1ed033392ac260ae06ece449b8034c2cbd810
Rafael Saca <raffaellsaca@gmail.com>
2024-06-25 21:41:47 -0300

    feat: adicionando classe container e container de conteudo na main do projeto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ToolbarComponent],
+  imports: [RouterOutlet,ToolbarComponent,HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
