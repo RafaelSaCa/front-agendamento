05e715057ad1a4343e039228652ae978d8539b33
Rafael Saca <raffaellsaca@gmail.com>
2024-06-28 22:19:02 -0300

    feat: adiciona botão para abrir dialog de formulario para cadastro de paciente

--- src/app/paciente/lista-paciente/lista-paciente.component.ts
+++ src/app/paciente/lista-paciente/lista-paciente.component.ts
@@ -1,30 +1,50 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import { HeaderComponent } from '../../shared/header/header.component';
+import { DialogFormComponent } from '../dialog-form/dialog-form.component';
 import { Paciente } from '../model/paciente';
 import { PacienteService } from '../services/paciente.service';
-import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-lista-paciente',
   standalone: true,
-  imports: [MatTableModule, HeaderComponent],
+  imports: [MatTableModule, HeaderComponent,MatDividerModule, MatButtonModule, MatIconModule, MatDialogModule],
   templateUrl: './lista-paciente.component.html',
   styleUrl: './lista-paciente.component.scss'
 })
 export class ListaPacienteComponent implements OnInit {
 
-  @Output() textoHeader =  'sfsdsdfsdf';
-
   pacientes: Paciente[] = [];
   displayedColumns: string[] = ['nome', 'sobrenome','cpf','email',]
 
-  constructor(private service: PacienteService){}
+  constructor(private service: PacienteService, public dialog: MatDialog){}
 
 
   ngOnInit(): void {
+    this.carregaDados();
+  }
+
+
+  addPaciente(): void {
+    const dialogRef = this.dialog.open(DialogFormComponent, {
+     minWidth: '580px',
+     minHeight: '580px',
+
+    });
+
+    dialogRef.afterClosed().subscribe(() => {
+      console.log('The dialog was closed');
+      this.carregaDados();
+    });
+  }
+
+  carregaDados(){
     this.service.listarTodos().subscribe(paciente =>{
       this.pacientes = paciente;
     });
   }
-
 }
