d1b3418099a7eb478e933e95d971279e2ab94f1b
Rafael Saca <raffaellsaca@gmail.com>
2024-07-06 17:33:51 -0300

    feat: adiciona footer a página

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { Router, RouterOutlet } from '@angular/router';
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,ToolbarComponent,HeaderComponent],
+  imports: [RouterOutlet,ToolbarComponent,HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
